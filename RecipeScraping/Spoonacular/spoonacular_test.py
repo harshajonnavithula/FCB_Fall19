7c3d8ccc6ad5369d31435e71022376eaf04d5511
Kaitlyn C <kaitlyn.commes@gmail.com>
2019-10-03 11:03:31 -0400

    Fixed spoonacular, added example csv's

--- RecipeScraping/Spoonacular/spoonacular_test.py
+++ RecipeScraping/Spoonacular/spoonacular_test.py
@@ -4,26 +4,40 @@ from pandas.io.json import json_normalize
 import pandas as pd
 import pathlib
 
+#API Key
 api = sp.API('a3dc0c4c374149b29dfba84a54dec14c')
 #zomato api key: 760e6dfad78b661b5a418129adbdb04c
 
-query = 'pasta'
-cuisine = 'Italian'
+#Output path setup
+parent = pathlib.Path.cwd() / 'RecipeScraping' / 'Spoonacular'
+outputpath = parent / 'spoonacular_output.json'
+csvpath = parent / 'spoondata_vegetarian.csv'
+
+#API Query Settings
+query = ''
+number = 10
 sort = 'popularity'
-number = 5
+offset = 10
+diet = 'vegetarian'
 
-response = api.search_recipes_complex(query, cuisine = cuisine, sort = sort, sortDirection = 'desc', number = number, addRecipeInformation = True)
+#Calling the API, sending results to JSON file and interpreting into dataframe
+response = api.search_recipes_complex(query, sort = sort,\
+ sortDirection = 'desc', number = number, addRecipeInformation = True,\
+ fillIngredients = True, diet = diet, offset = offset)
 data = response.json()
-
-outputfile = open('spoonacular_output.json', 'w')
+outputfile = open(outputpath, 'w')
 json.dump(data,outputfile, indent=4)
 outputfile.close()
-
 spoondata = json_normalize(data['results'])
 
-with open('spoonacular_output.json', 'r') as datafile:
-    test = json.load(datafile)
+#Reading existing data already loaded into csv
+with open(csvpath, 'r') as datafile:
+    read = pd.read_csv(datafile, index_col=0)
     datafile.close()
-df2 = json_normalize(test['results'])
 
-spoondata.to_excel('spoondata.xlsx')
+#Appending new data to existing data, duplicates dropped
+combineddata = read.append(spoondata, sort=False)
+combineddata = combineddata.drop_duplicates('id').reset_index(drop=True)
+
+#Output to csv
+combineddata.to_csv(csvpath)
